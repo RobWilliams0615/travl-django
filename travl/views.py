904fa8123dfaa4ad9ba74cab0989f30b95f37922
RicoWilliams <ricowilliams@protonmail.com>
2022-02-07 11:56:11 -0700

    facility list, facilitydetail, locaitonlist, locationdetail views created

--- travl/views.py
+++ travl/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from travl.models import Facility, Location
+
 # Create your views here.
+
+
+class FacilityList(generics.ListCreateAPIView):
+    queryset = Facility.objects.all()
+    serializer_class = FacilitySerializer
+
+
+class FacilityDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Facility.objects.all()
+    serializer_class = FacilitySerializer
+
+
+class LocationList(generics.ListCreateAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
